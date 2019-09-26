aca77a6a500d00b0f346973c81ce5b839b6826a7
sakthirajan <sakthirajan.sembulingam@gmail.com>
2019-09-26 18:39:34 +0530

    2 files modified

--- index.js
+++ index.js
@@ -95,3 +95,27 @@ Cylinder.prototype.Volume = function () {
 var cyl = new Cylinder(7, 4);
 console.log("Volume = ", cyl.Volume().toFixed(4)); // for 4 digit
 
+const six = document.getElementById('6');
+six.innerHTML = `6.)Write a Bubble Sort algorithm in JavaScript.
+Note: Bubble sort is a simple sorting algorithm that works by repeatedly stepping through the list to be sorted,
+Sample Data: [6,4,0, 3,-2,1]
+Expected Output : [-2, 0, 1, 3, 4, 6]`;
+
+const seven = document.getElementById('7');
+seven.innerHTML = `7.)Write a JavaScript program which returns a subset of a string.
+Sample Data: dog
+Expected Output: ["d", "do", "dog", "o", "og", "g"]`;
+
+
+function subSet(str) {
+  var _subset = [];
+  for (var m = 0; str.length > m; m++) {
+    for () {
+
+    }
+  }
+}
+
+
+
+
